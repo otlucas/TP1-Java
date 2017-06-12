c660d625d4b8ae9867edf38153b2d8c3e35560d1
otlucas <otero.lucas@outlook.com>
2017-06-12 09:56:05 -0300

    Create Conversacion.java

--- TP1-Java/src/AlgoChat/Conversacion.java
+++ TP1-Java/src/AlgoChat/Conversacion.java
@@ -27,9 +27,7 @@ public class Conversacion {
 
 	public void agregarMensaje(Mensaje nuevoMensaje) {
 		/** Agrega un mensaje a la lista de mensajes */
-		
 		this.mensajes.add(nuevoMensaje);
-		
 	}
 
 	public int cantidadMensajesEnviados() {
@@ -55,10 +53,8 @@ public class Conversacion {
 		}
 		listaDeMensajes.add(0, "");
 		return listaDeMensajes;
-		
 	}
-		
-
+	
 	public String devolverNombreDeGrupo() {
 		/** Devuelve el nombre del grupo con el cual se tiene la conversacion */
 		return this.grupo.devolverNombre();
@@ -77,8 +73,6 @@ public class Conversacion {
 		
 		}
 		this.mensajes = new ArrayList<Mensaje>();
-			
-		
 	}
 
 	public void borrarConversacionGrupal(Usuario usuarioprincipal, Grupo grupo) {
@@ -94,18 +88,5 @@ public class Conversacion {
 		
 		}
 		this.mensajes = new ArrayList<Mensaje>();
-		
 	}	
-
 }
-
-
-
-
-
-
-
-
-
-
-

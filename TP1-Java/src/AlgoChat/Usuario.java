22dc8f474936b757eb43ac1bd71ebdb79fbbbc33
otlucas <otero.lucas@outlook.com>
2017-06-12 09:58:06 -0300

    Create Usuario.java

--- TP1-Java/src/AlgoChat/Usuario.java
+++ TP1-Java/src/AlgoChat/Usuario.java
@@ -11,14 +11,13 @@ public class Usuario {
 		this.nombre = nombredeusuario;
 		this.cantidadmensajesenviados = 0;
 		this.cantidadmensajesrecibidos = 0;
-		
 	}
 
 	public int cantidadDeMensajesEnviados() {
 		/** Devuelve la cantidad de mensajes enviados del usuario */
 		return this.cantidadmensajesenviados;
 	}
-
+	
 	public String devolverNombre() {
 		/** Devuelve el nombre del usuario */
 		return this.nombre;
@@ -26,21 +25,16 @@ public class Usuario {
 
 	public void agregarEnviado() {
 		/** Agrega 1 a la cantidad de mensajes enviados */
-		
 		this.cantidadmensajesenviados += 1;
-		
 	}
 
 	public void agregarRecibido() {
 		/** Agrega 1 a la cantidad de mensajes recibidos */
-		
-		this.cantidadmensajesrecibidos += 1;
-		
+		this.cantidadmensajesrecibidos += 1;	
 	}
 
 	public int cantidadTotalMensajesRecibidos() {
 		/** Devuelve la cantidad de mensajes recibidos del usuario */
 		return this.cantidadmensajesrecibidos;
 	}
-
 }

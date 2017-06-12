bf20d3442b40c340b96bd5f4007afab27bb5eff9
otlucas <otero.lucas@outlook.com>
2017-06-12 10:04:07 -0300

    Create AlgoChatTest.java

--- TP1-Java/test/fiuba/algo3/AlgoChatTest.java
+++ TP1-Java/test/fiuba/algo3/AlgoChatTest.java
@@ -15,10 +15,10 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
         
         try {
-			algoChat.agregarContacto("Roberto");
-		} catch (ContactoYaExisteExcepcion e) {
-			e.printStackTrace();
-		}
+		algoChat.agregarContacto("Roberto");
+	} catch (ContactoYaExisteExcepcion e) {
+		e.printStackTrace();
+	}
 
         assertEquals(1, algoChat.cantidadDeContactos());
         assertEquals("Lucas", algoChat.nombreUsuario());
@@ -30,16 +30,15 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
-			algoChat.crearGrupo("Mañana");
-			algoChat.agregarContactoAGrupo("Agustin", "Mañana");
+		algoChat.agregarContacto("Agustin");
+		algoChat.crearGrupo("Mañana");
+		algoChat.agregarContactoAGrupo("Agustin", "Mañana");
 			
-			assertEquals(1, algoChat.cantidadDeContactos());
-			assertEquals(2, algoChat.cantidadMiembrosEnGrupo("Mañana"));
+		assertEquals(1, algoChat.cantidadDeContactos());
+		assertEquals(2, algoChat.cantidadMiembrosEnGrupo("Mañana"));
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	} catch (Exception e) {
+	}
     }
 
     @Test
@@ -48,23 +47,22 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Marcio");
+		algoChat.agregarContacto("Marcio");
 		
-			algoChat.recibirMensajeDe("Marcio", "Estas en tu casa?");
-			algoChat.recibirMensajeDe("Marcio", "HOLA?");
+		algoChat.recibirMensajeDe("Marcio", "Estas en tu casa?");
+		algoChat.recibirMensajeDe("Marcio", "HOLA?");
 		
 
-			assertEquals(2, algoChat.cantidadTotalMensajesRecibidos());
-			assertEquals(2, algoChat.cantidadMensajesDe("Marcio"));
+		assertEquals(2, algoChat.cantidadTotalMensajesRecibidos());
+		assertEquals(2, algoChat.cantidadMensajesDe("Marcio"));
 
-			algoChat.borrarMensajesDelContacto("Marcio");
+		algoChat.borrarMensajesDelContacto("Marcio");
 		
-			assertEquals(0, algoChat.cantidadTotalMensajesRecibidos());
-			assertEquals(0, algoChat.cantidadMensajesDe("Marcio"));
+		assertEquals(0, algoChat.cantidadTotalMensajesRecibidos());
+		assertEquals(0, algoChat.cantidadMensajesDe("Marcio"));
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	} catch (Exception e) {
+	}
     }
 
     @Test
@@ -73,18 +71,17 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.recibirMensajeDe("Agustin", "Estas?");
-			algoChat.enviarMensajeA("Agustin", "Si.");
-			algoChat.enviarMensajeA("Agustin", "Que pasa?");
+		algoChat.recibirMensajeDe("Agustin", "Estas?");
+		algoChat.enviarMensajeA("Agustin", "Si.");
+		algoChat.enviarMensajeA("Agustin", "Que pasa?");
 
 
-			assertEquals("Yo: Que pasa?", algoChat.obtenerConversacionCon("Agustin").get(1));
-			assertEquals("Yo: Si.", algoChat.obtenerConversacionCon("Agustin").get(2));
-			assertEquals("Agustin: Estas?", algoChat.obtenerConversacionCon("Agustin").get(3));
+		assertEquals("Yo: Que pasa?", algoChat.obtenerConversacionCon("Agustin").get(1));
+		assertEquals("Yo: Si.", algoChat.obtenerConversacionCon("Agustin").get(2));
+		assertEquals("Agustin: Estas?", algoChat.obtenerConversacionCon("Agustin").get(3));
         } catch (Exception e) {
-			e.printStackTrace();
 		}
     }
 
@@ -94,28 +91,27 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Tomas");
 
-			algoChat.crearGrupo("Mañana");
-			algoChat.agregarContactoAGrupo("Agustin", "Mañana");
-			algoChat.agregarContactoAGrupo("Tomas", "Mañana");
+		algoChat.crearGrupo("Mañana");
+		algoChat.agregarContactoAGrupo("Agustin", "Mañana");
+		algoChat.agregarContactoAGrupo("Tomas", "Mañana");
 
-			algoChat.recibirMensajeDeGrupo("Mañana", "Agustin", "Hola");
-			algoChat.recibirMensajeDeGrupo("Mañana", "Tomas", "Que haces");
-			algoChat.enviarMensajeAGrupo("Mañana", "Que haces");
+		algoChat.recibirMensajeDeGrupo("Mañana", "Agustin", "Hola");
+		algoChat.recibirMensajeDeGrupo("Mañana", "Tomas", "Que haces");
+		algoChat.enviarMensajeAGrupo("Mañana", "Que haces");
 
-			assertEquals(2, algoChat.cantidadMensajesRecibidosDelGrupo("Mañana"));
-			assertEquals(1, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
+		assertEquals(2, algoChat.cantidadMensajesRecibidosDelGrupo("Mañana"));
+		assertEquals(1, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
         
-			algoChat.borrarMensajesDelGrupo("Mañana");
+		algoChat.borrarMensajesDelGrupo("Mañana");
 			
-			assertEquals(0, algoChat.cantidadMensajesRecibidosDelGrupo("Mañana"));
-			assertEquals(0, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
+		assertEquals(0, algoChat.cantidadMensajesRecibidosDelGrupo("Mañana"));
+		assertEquals(0, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
 			
         } catch (Exception e) {
-			e.printStackTrace();
 		}
     }
     
@@ -125,17 +121,16 @@ public class AlgoChatTest {
     	AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.recibirMensajeDe("Agustin", "Estas?");
-			algoChat.enviarMensajeA("Agustin", "Si.");
-			algoChat.enviarMensajeA("Agustin", "Que pasa?");
-			algoChat.recibirMensajeDe("Agustin", "Nada");
+		algoChat.recibirMensajeDe("Agustin", "Estas?");
+		algoChat.enviarMensajeA("Agustin", "Si.");
+		algoChat.enviarMensajeA("Agustin", "Que pasa?");
+		algoChat.recibirMensajeDe("Agustin", "Nada");
         
-			assertEquals(2, algoChat.cantidadMensajesDe("Agustin"));
+		assertEquals(2, algoChat.cantidadMensajesDe("Agustin"));
 			
         } catch (Exception e) {
-			e.printStackTrace();
 		}
     }
     
@@ -146,18 +141,17 @@ public class AlgoChatTest {
     	AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.recibirMensajeDe("Agustin", "Estas?");
-			algoChat.enviarMensajeA("Agustin", "Si.");
-			algoChat.enviarMensajeA("Agustin", "Que pasa?");
+		algoChat.recibirMensajeDe("Agustin", "Estas?");
+		algoChat.enviarMensajeA("Agustin", "Si.");
+		algoChat.enviarMensajeA("Agustin", "Que pasa?");
         	algoChat.recibirMensajeDe("Agustin", "Nada");
         
         	assertEquals(2, algoChat.cantidadMensajesEnviadosA("Agustin"));
         
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -166,20 +160,19 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
-		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Agustin");
+	
+		algoChat.agregarContacto("Tomas");
 
-			algoChat.crearGrupo("Mañana");
-			algoChat.enviarMensajeAGrupo("Mañana", "A");
-			algoChat.enviarMensajeAGrupo("Mañana", "A");
-			algoChat.enviarMensajeAGrupo("Mañana", "A");
+		algoChat.crearGrupo("Mañana");
+		algoChat.enviarMensajeAGrupo("Mañana", "A");
+		algoChat.enviarMensajeAGrupo("Mañana", "A");
+		algoChat.enviarMensajeAGrupo("Mañana", "A");
 
-			assertEquals(3, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
+		assertEquals(3, algoChat.cantidadMensajesEnviadosAlGrupo("Mañana"));
 			
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -188,7 +181,7 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
         	algoChat.agregarContacto("Tomas");
 
         	algoChat.crearGrupo("Mañana");
@@ -201,7 +194,6 @@ public class AlgoChatTest {
         	assertEquals(2, algoChat.cantidadMensajesRecibidosDelGrupo("Mañana"));
         	
         } catch (Exception e) {
-			e.printStackTrace();
         }
     }
     
@@ -211,9 +203,9 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Tomas");
 
         	algoChat.crearGrupo("Mañana");
         	algoChat.agregarContactoAGrupo("Agustin", "Mañana");
@@ -226,8 +218,7 @@ public class AlgoChatTest {
         	assertEquals("Tomas: Que haces", algoChat.obtenerConversacionConGrupo("Mañana").get(1));
         
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -236,24 +227,23 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
-		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Agustin");
+	
+		algoChat.agregarContacto("Tomas");
 
-			algoChat.crearGrupo("Mañana");
-			algoChat.agregarContactoAGrupo("Agustin", "Mañana");
-			algoChat.agregarContactoAGrupo("Tomas", "Mañana");
+		algoChat.crearGrupo("Mañana");
+		algoChat.agregarContactoAGrupo("Agustin", "Mañana");
+		algoChat.agregarContactoAGrupo("Tomas", "Mañana");
 
-			algoChat.recibirMensajeDeGrupo("Mañana", "Agustin", "Hola");
-			algoChat.recibirMensajeDeGrupo("Mañana", "Tomas", "Que haces");
-			algoChat.enviarMensajeAGrupo("Mañana", "A");
-			algoChat.enviarMensajeAGrupo("Mañana", "A");
+		algoChat.recibirMensajeDeGrupo("Mañana", "Agustin", "Hola");
+		algoChat.recibirMensajeDeGrupo("Mañana", "Tomas", "Que haces");
+		algoChat.enviarMensajeAGrupo("Mañana", "A");
+		algoChat.enviarMensajeAGrupo("Mañana", "A");
 
-			assertEquals("Yo: A", algoChat.obtenerConversacionConGrupo("Mañana").get(2));
-			assertEquals("Yo: A", algoChat.obtenerConversacionConGrupo("Mañana").get(1));
+		assertEquals("Yo: A", algoChat.obtenerConversacionConGrupo("Mañana").get(2));
+		assertEquals("Yo: A", algoChat.obtenerConversacionConGrupo("Mañana").get(1));
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -262,16 +252,15 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Tomas");
 
         	algoChat.crearGrupo("Mañana");
 
         	assertTrue(algoChat.existeGrupo("Mañana"));
         	
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -280,17 +269,16 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.enviarMensajeA("Agustin", "Si.");
+		algoChat.enviarMensajeA("Agustin", "Si.");
         	algoChat.enviarMensajeA("Agustin", "Que pasa?");
 
         	assertEquals("Yo: Que pasa?", algoChat.obtenerConversacionCon("Agustin").get(1));
         	assertEquals("Yo: Si.", algoChat.obtenerConversacionCon("Agustin").get(2));
         
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -299,17 +287,16 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
 
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.recibirMensajeDe("Agustin", "Estas?");
+		algoChat.recibirMensajeDe("Agustin", "Estas?");
         	algoChat.recibirMensajeDe("Agustin", "Estas?");
         
         	assertEquals("Agustin: Estas?", algoChat.obtenerConversacionCon("Agustin").get(1));
         	assertEquals("Agustin: Estas?", algoChat.obtenerConversacionCon("Agustin").get(2));
         
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -318,9 +305,9 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
         
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Tomas");
 
         	algoChat.crearGrupo("Mañana");
         	algoChat.agregarContactoAGrupo("Agustin", "Mañana");
@@ -337,8 +324,7 @@ public class AlgoChatTest {
         	assertEquals(4, algoChat.cantidadTotalMensajesRecibidos());
         
         } catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
     }
     
     @Test
@@ -347,9 +333,9 @@ public class AlgoChatTest {
         AlgoChat algoChat = new AlgoChat("Lucas");
         
         try {
-			algoChat.agregarContacto("Agustin");
+		algoChat.agregarContacto("Agustin");
 		
-			algoChat.agregarContacto("Tomas");
+		algoChat.agregarContacto("Tomas");
 
         	algoChat.crearGrupo("Mañana");
         	algoChat.agregarContactoAGrupo("Agustin", "Mañana");
@@ -365,7 +351,6 @@ public class AlgoChatTest {
         	assertEquals(4, algoChat.cantidadDeMensajesEnviados());
         
         } catch (Exception e) {
-        	e.printStackTrace();
-		}
+	}
     }   
 }
